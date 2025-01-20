1ec20c9c324dbaab73b85ab76a919fc6ff9bba48
Anca Lumezeanu <anca.lumezeanu40@thg.com>
2025-01-20 15:44:29 +0000

    fixed ai class name

--- src/main/java/com/thg/accelerator23/connectn/ai/zugzwang_dominators/ZugzwangDominator.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/zugzwang_dominators/ZugzwangDominator.java
@@ -5,14 +5,15 @@ import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
 
-public class NameAi extends Player {
-  public NameAi(Counter counter) {
+public class ZugzwangDominator extends Player {
+  public ZugzwangDominator(Counter counter) {
     //TODO: fill in your name here
-    super(counter, NameAi.class.getName());
+    super(counter, ZugzwangDominator.class.getName());
   }
 
   @Override
   public int makeMove(Board board) {
+
     //TODO: some crazy analysis
     //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
     return 4;
