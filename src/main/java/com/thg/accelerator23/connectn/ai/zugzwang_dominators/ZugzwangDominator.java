f66dbaae5ecc7bc24f99abc000de6324327e374a
Anca Lumezeanu <anca.lumezeanu40@thg.com>
2025-01-20 20:03:22 +0000

    added make move with random

--- src/main/java/com/thg/accelerator23/connectn/ai/zugzwang_dominators/ZugzwangDominator.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/zugzwang_dominators/ZugzwangDominator.java
@@ -3,6 +3,7 @@ package com.thg.accelerator23.connectn.ai.zugzwang_dominators;
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
+import com.thehutgroup.accelerator.connectn.player.Position;
 
 
 public class ZugzwangDominator extends Player {
@@ -13,9 +14,15 @@ public class ZugzwangDominator extends Player {
 
   @Override
   public int makeMove(Board board) {
+    int column = (int) (Math.random() * board.getConfig().getWidth());
+
+    if (board.getCounterAtPosition(new Position(column, board.getConfig().getHeight())) == null) {
+      return column;
+    } else {
+      return makeMove(board);
+    }
 
     //TODO: some crazy analysis
     //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
-    return 4;
   }
 }
